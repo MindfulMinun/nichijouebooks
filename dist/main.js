ca642fe514d5da0740060d6002212aab29b2cf53
MindfulMinun <importantmails4bc@gmail.com>
2019-03-24 01:07:07 -0500

    Interstitials!
    Running `yarn interstitial` or `node dist/main.js -i`
    will tweet an interstitial.

--- dist/main.js
+++ dist/main.js
@@ -1,5 +1,20 @@
 'use strict';
 
+var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }(); // path/fs: To fetch the interstitials
+// Twit: the library for connecting to Twitter
+// Markov: the Markov chain text generator
+// SelamatPagi: the dictionary with all the Nichijou quotes
+// that Markov will generate from.
+
+
+var _path = require('path');
+
+var _path2 = _interopRequireDefault(_path);
+
+var _fs = require('fs');
+
+var _fs2 = _interopRequireDefault(_fs);
+
 var _twit = require('twit');
 
 var _twit2 = _interopRequireDefault(_twit);
@@ -14,12 +29,10 @@ var _NichijouDict2 = _interopRequireDefault(_NichijouDict);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var production = /production/i.test(process.env.NODE_ENV || ''); // Twit: the library for connecting to Twitter
-// Markov: the Markov chain text generator
-// SelamatPagi: the dictionary with all the Nichijou quotes
-// that Markov will generate from.
-
+var production = /production/i.test(process.env.NODE_ENV || '');
 require('dotenv').config();
+
+var args = process.argv.slice(2);
 
 // Crow: The Twit client instance
 // Reference to the talking crow in Nichijou E17
@@ -28,34 +41,123 @@ var Crow = new _twit2.default({
     consumer_secret: process.env.C_SECRET,
     access_token: process.env.A_TOKEN,
     access_token_secret: process.env.A_SECRET,
-    timeout_ms: 60 * 1000,
+    timeout_ms: 30 * 1000,
     strictSSL: true
 });
 
-// Nano: The Markov generator instance
-var Nano = new _markovStrings2.default(_NichijouDict2.default, { stateSize: 1 });
-Nano.buildCorpus();
+if (args.some(function (v) {
+    return ['-i', '--interstitial', '--interstitials'].includes(v);
+})) {
+    // Make an interstitial!
+    // Import the interstitials data
+    var data = require(_path2.default.resolve(__dirname, '../interstitials/info.json'));
 
-var NanoOptions = {
-    maxTries: 100,
-    filter: function filter(result) {
-        return [
-        // 1 < result.refs.length,
-        10 < result.score].every(function (v) {
-            return v;
+    // Choose an image, get its alt text data
+
+    var _chooseObject = chooseObject(data),
+        _chooseObject2 = _slicedToArray(_chooseObject, 2),
+        name = _chooseObject2[0],
+        alt = _chooseObject2[1];
+
+    alt = alt.alt;
+    var dir = _path2.default.resolve(__dirname, '../interstitials/');
+    var file = _fs2.default.readdirSync(dir).filter(function (f) {
+        return f.startsWith(name);
+    }).map(function (f) {
+        return _path2.default.resolve(__dirname, '../interstitials/', f);
+    });
+    file = _fs2.default.readFileSync(choose(file), { encoding: 'base64' });
+
+    if (production) {
+        Crow.post('media/upload', { media_data: file }, function (err, data) {
+            // Ignore 131 errors lmao
+            if (err && err.code !== 131) throw err;
+
+            console.log('Uploaded image data:', data);
+            var mediaMeta = {
+                media_id: data.media_id_string,
+                alt_text: { text: alt }
+            };
+
+            Crow.post('media/metadata/create', mediaMeta, function (err, data, resp) {
+                // Ignore 131 errors lmao
+                if (err && err.code !== 131) throw err;
+
+                var meta = {
+                    status: 'Episode ' + name.split('-')[0],
+                    media_ids: [mediaMeta.media_id]
+                };
+
+                Crow.post('statuses/update', meta).then(function (data) {
+                    console.log('Success!');
+                }).catch(function (err) {
+                    // There would be an error thrown here if something went wrong.
+                    // No clue why Twitter throws 131 errors out of the blue lmao
+                    console.log('Error upon posting', err);
+                });
+            });
         });
     }
-
-    // Generate a sentence and send it to Twitter.
-};var phrase = Nano.generate(NanoOptions);
-
-if (production) {
-    Crow.post('statuses/update', { status: phrase.string }).then(function (resp) {
-        console.log('Tweeted successfully.', phrase);
-    }).catch(function (err) {
-        console.log('Something happened, couldn’t post tweet.');
-        console.log(err);
-    });
 } else {
+    // Otherwise, simply generate a tweet.
+    // Nano: The Markov generator instance
+    var Nano = new _markovStrings2.default(_NichijouDict2.default, { stateSize: 1 });
+    Nano.buildCorpus();
+
+    var NanoOptions = {
+        maxTries: 100,
+        filter: function filter(result) {
+            return [
+            // 1 < result.refs.length,
+            10 < result.score].every(function (v) {
+                return v;
+            });
+        }
+
+        // Generate a sentence and send it to Twitter.
+    };var phrase = Nano.generate(NanoOptions);
+
+    if (production) {
+        Crow.post('statuses/update', { status: phrase.string }).then(function (resp) {
+            console.log('Tweeted successfully.');
+        }).catch(function (err) {
+            console.log('Something happened, couldn’t post tweet.');
+            console.log(err);
+        });
+    }
     console.log(phrase);
+}
+
+// ========================================
+// helpers ahh
+/**
+ * Chooses a key/value pair from an object at random
+ * @author MindfulMinun
+ * @param {Object} object - The object to choose from
+ * @returns {Array} - The key/value pair
+ * @since 20 Mar 2019
+ * @version 0.1.0
+**/
+function chooseObject(object) {
+    var keys = [];
+    for (var key in object) {
+        if (object.hasOwnProperty(key)) {
+            // ‘Cause you gotta be sure it’s in there
+            keys.push(key);
+        }
+    }
+    var selected = choose(keys);
+    return [selected, object[selected]];
+}
+
+/**
+ * Chooses an element from an array at random
+ * @author MindfulMinun
+ * @param {Array} arr - The array to choose from
+ * @returns {*} - The randomly-chosen element
+ * @since 20 Mar 2019
+ * @version 0.1.0
+**/
+function choose(arr) {
+    return arr[~~(Math.random() * arr.length)];
 }
--- src/main.js
+++ src/main.js
@@ -1,13 +1,18 @@
+// path/fs: To fetch the interstitials
 // Twit: the library for connecting to Twitter
 // Markov: the Markov chain text generator
 // SelamatPagi: the dictionary with all the Nichijou quotes
 // that Markov will generate from.
+import path from 'path'
+import fs from 'fs'
 import Twit from 'twit'
 import Markov from 'markov-strings'
 import SelamatPagi from './NichijouDict.js'
 
 const production = /production/i.test(process.env.NODE_ENV || '')
 require('dotenv').config()
+
+const args = process.argv.slice(2)
 
 // Crow: The Twit client instance
 // Reference to the talking crow in Nichijou E17
@@ -16,36 +21,122 @@ const Crow = new Twit({
     consumer_secret:      process.env.C_SECRET,
     access_token:         process.env.A_TOKEN,
     access_token_secret:  process.env.A_SECRET,
-    timeout_ms:           60 * 1000,
+    timeout_ms:           30 * 1000,
     strictSSL:            true
 })
 
-// Nano: The Markov generator instance
-const Nano = new Markov(SelamatPagi, { stateSize: 1 })
-Nano.buildCorpus()
+if (args.some(v => ['-i', '--interstitial', '--interstitials'].includes(v))) {
+    // Make an interstitial!
+    // Import the interstitials data
+    const data = require(
+        path.resolve(__dirname, '../interstitials/info.json')
+    )
 
-const NanoOptions = {
-    maxTries: 100,
-    filter: (result) => {
-        return [
-            // 1 < result.refs.length,
-            10 < result.score
-        ].every(v => v)
+    // Choose an image, get its alt text data
+    var [name, alt] = chooseObject(data)
+    alt = alt.alt
+    var dir = path.resolve(__dirname, '../interstitials/')
+    var file = fs.readdirSync(dir)
+        .filter((f) => f.startsWith(name))
+        .map((f) => path.resolve(__dirname, '../interstitials/', f))
+    file = fs.readFileSync(choose(file), { encoding: 'base64' })
+
+    if (production) {
+        Crow.post('media/upload', { media_data: file }, (err, data) => {
+            // Ignore 131 errors lmao
+            if (err && err.code !== 131) throw err
+
+            console.log('Uploaded image data:', data)
+            var mediaMeta = {
+                media_id: data.media_id_string,
+                alt_text: { text: alt }
+            }
+
+            Crow.post('media/metadata/create', mediaMeta, (err, data, resp) => {
+                // Ignore 131 errors lmao
+                if (err && err.code !== 131) throw err
+
+                var meta = {
+                    status: `Episode ${name.split('-')[0]}`,
+                    media_ids: [mediaMeta.media_id]
+                }
+
+                Crow.post('statuses/update', meta)
+                .then(data => {
+                    console.log('Success!')
+                })
+                .catch(err => {
+                    // There would be an error thrown here if something went wrong.
+                    // No clue why Twitter throws 131 errors out of the blue lmao
+                    console.log('Error upon posting', err)
+                })
+            })
+
+        })
     }
+
+} else {
+    // Otherwise, simply generate a tweet.
+    // Nano: The Markov generator instance
+    const Nano = new Markov(SelamatPagi, { stateSize: 1 })
+    Nano.buildCorpus()
+
+    const NanoOptions = {
+        maxTries: 100,
+        filter: (result) => {
+            return [
+                // 1 < result.refs.length,
+                10 < result.score
+            ].every(v => v)
+        }
+    }
+
+    // Generate a sentence and send it to Twitter.
+    let phrase = Nano.generate(NanoOptions)
+
+    if (production) {
+        Crow.post('statuses/update', { status: phrase.string })
+            .then(resp => {
+                console.log('Tweeted successfully.')
+            })
+            .catch(err => {
+                console.log('Something happened, couldn’t post tweet.')
+                console.log(err)
+            })
+    }
+    console.log(phrase)
 }
 
-// Generate a sentence and send it to Twitter.
-let phrase = Nano.generate(NanoOptions)
+// ========================================
+// helpers ahh
+/**
+ * Chooses a key/value pair from an object at random
+ * @author MindfulMinun
+ * @param {Object} object - The object to choose from
+ * @returns {Array} - The key/value pair
+ * @since 20 Mar 2019
+ * @version 0.1.0
+**/
+function chooseObject(object) {
+    var keys = []
+    for (var key in object) {
+        if (object.hasOwnProperty(key)) {
+            // ‘Cause you gotta be sure it’s in there
+            keys.push(key)
+        }
+    }
+    var selected = choose(keys);
+    return [selected, object[selected]]
+}
 
-if (production) {
-    Crow.post('statuses/update', { status: phrase.string })
-        .then(resp => {
-            console.log('Tweeted successfully.', phrase)
-        })
-        .catch(err => {
-            console.log('Something happened, couldn’t post tweet.')
-            console.log(err)
-        })
-} else {
-    console.log(phrase)
+/**
+ * Chooses an element from an array at random
+ * @author MindfulMinun
+ * @param {Array} arr - The array to choose from
+ * @returns {*} - The randomly-chosen element
+ * @since 20 Mar 2019
+ * @version 0.1.0
+**/
+function choose(arr) {
+    return arr[~~(Math.random() * arr.length)]
 }
